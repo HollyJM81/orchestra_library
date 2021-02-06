7343c5ebb0e756250365ced5b0289e376b81c12f
Holly Mathieson <66640795+HollyJM81@users.noreply.github.com>
2021-02-06 12:31:26 +0000

    create composer directory and add sql files

--- createComposerTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE composers (
-    id serial PRIMARY KEY,
-    last_name VARCHAR(50) NOT NULL,
-    middle_name VARCHAR(50),
-    first_name VARCHAR(50) NOT NULL,
-    date_of_birth DATE NOT NULL
-);
--- insertComposer.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO composers (last_name, first_name, date_of_birth)
-VALUES ('Mendelssohn', 'Fanny', '1805-11-14');
